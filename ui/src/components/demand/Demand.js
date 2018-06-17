089bb03ff19d7b6f2695d5146ba387ef87c73041
gideon.potok <gideon.raphael.potok@ibm.com>
2018-06-17 16:19:52 -0400

    changed demand component so that it fetches demands from rest layer, stores it in state,  and displays it when curSection == All.

--- ui/src/components/demand/Demand.js
+++ ui/src/components/demand/Demand.js
@@ -2,32 +2,38 @@ import React, { Component } from 'react';
 import './Demand.css';
 import Builder from '../builder/Builder';
 
+
 export default class Demand extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      curSection: '',
-      pictures: [],
+      curSection: 'All',
+      stateArrOfDemands: [],
     };
   }
   componentDidMount() {
   fetch('http://localhost:8080/demands')
   .then(results => {
     return results.json();
-    }).then(data => {
-        let pictures = data.map((pic) => {
+    }).then(arrOfDemands => {
+        let localArrayOfDemands = arrOfDemands.map((eachDemand) => {
             return (
-            <div>
-             <div>demand: {pic}</div>
+            <div key={eachDemand.bu}>
+             <h2> {eachDemand.bu}, {eachDemand.location}, {eachDemand.num_tickets}</h2>
+             <h2> {eachDemand.onboarding_quarter}, {eachDemand.req_title} </h2>
+             <h2> {eachDemand.specialty}, {eachDemand.track}</h2>
              </div>
             )
-         })
-        this.setState({picture:pictures});
-        console.log("state was", this.state.pictures);
+         });
+
+        this.setState({ curSection: this.state.curSection, stateArrOfDemands : localArrayOfDemands});
+
+
     })
 
   }
   render() {
+   if(this.state.curSection == ' '){
     return(
       <div className="demand">
         <div className="demand-navbar">
@@ -42,11 +48,32 @@ export default class Demand extends Component {
           <h2 className="demand-content-title">{this.state.curSection}</h2>
           <hr />
           {/* <DataTable></DataTable> */}
-          {this.state.pictures}
           <Builder></Builder>
         </div>
       </div>
     );
+    } else{
+        if(this.state.curSection == 'All'){
+        return(
+              <div className="demand">
+                <div className="demand-navbar">
+                  <div className="demand-navbar-title">Demand</div>
+                  <ul className="demand-ul">
+                    <li className="demand-li">All</li>
+                    <li className="demand-li">Drafts</li>
+                    <li className="demand-li">Builder</li>
+                  </ul>
+                </div>
+                <div className="demand-content">
+                  <h2 className="demand-content-title">{this.state.curSection}</h2>
+
+                  <hr />
+                    <h2> {this.state.stateArrOfDemands}</h2>
+                </div>
+              </div>
+            );
+        }
+    }
   }
 }
 
