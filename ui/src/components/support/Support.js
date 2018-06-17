ded258097a16908a973879a9b69bf931b2fd5726
Alexander Fong <alpfong@gmail.com>
2018-06-17 16:34:03 -0400

    Implement basic Support page

--- ui/src/components/support/Support.js
+++ ui/src/components/support/Support.js
@@ -11,7 +11,24 @@ export default class Support extends Component {
   render() {
     return(
       <div className="support">
-        Support
+        <div className="support-section">
+          <div className="support-section-title">Contact</div>
+          <div className="support-item">
+            <div className="support-item-title">For any inquiries not answered by the FAQs below, please contact us at</div>
+            <div className="support-item-desc">xcedrin.support@ibm.com</div>
+          </div>
+        </div>
+        <div className="support-section">
+          <div className="support-section-title">FAQ</div>
+          <div className="support-item">
+            <div className="support-item-title">Is there a limit on how much demand I can submit?</div>
+            <div className="support-item-desc">No, you may submit as much demand as necessary. However, please submit appropriate demand based on budget and management feedback.</div>
+          </div>
+          <div className="support-item">
+            <div className="support-item-title">How will I know if management has been notified of my demand submission?</div>
+            <div className="support-item-desc">An email alert will be automatically sent out to your Hiring Manager, VP, and other stakeholders when demand has been submitted.</div>
+          </div>
+        </div>
       </div>
     );
   }
