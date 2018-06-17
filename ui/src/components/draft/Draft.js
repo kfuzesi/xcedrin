de0fcd0745e0fe301d038a8317d6bfa232987310
Alexander Fong <alpfong@gmail.com>
2018-06-17 16:30:02 -0400

    Create Draft component

---
+++ ui/src/components/draft/Draft.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Draft.css';
+
+export default class Draft extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return(
+      <div className="draft">
+        Draft
+      </div>
+    )
+  }
+}
